9e017672fa2edfa126d2b629ffe204d29d563fa0
zamboni <luizamboni2002@hotmail.com>
2019-11-19 15:08:15 -0300

    ADD context

---
+++ 7-concurrency/5-select.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	c1 := make(chan int)
+	c2 := make(chan int)
+	count := 0
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- 1
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- 2
+	}()
+
+	for count < 2 {
+		fmt.Println("waiting channels")
+		// select is usad to wait multiple channels
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+			count++
+
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+			count++
+		}
+	}
+}
